f03a9bdb41079ae010923288e80ac6bffe666fc8
dejourford <dejourford@my.unt.edu>
2023-02-03 23:34:39 -0600

    style modals and create open and close function

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
 //  ==== define variables   ====    //
-const addBtn = document.querySelector('.addBtn')
+const addPokemonBtn = document.querySelector('.addPokemonBtn')
+const closeBtn = document.querySelector('.closeBtn')
+const resetBtn = document.querySelector('.resetBtn')
+const deleteCloseBtn = document.querySelector('.deleteCloseBtn')
+const showCloseBtn = document.querySelector('.showCloseBtn')
+const resetCloseBtn = document.querySelector('.resetCloseBtn')
 const deleteBtn = document.querySelector('.deleteBtn')
 const showBtn = document.querySelector('.showBtn')
+const showAllBtn = document.querySelector('#showAllBtn')
 const pokemonCard = document.querySelector('.pokemonCard')
 const pokemonName = document.querySelector('.pokemonName')
 const pokemonImg = document.querySelector('.pokemonImg')
-
+const addBtnModal = document.querySelector('.addBtnModal')
+const deleteBtnModal = document.querySelector('.deleteBtnModal')
+const showBtnModal = document.querySelector('.showBtnModal')
+const resetBtnModal =  document.querySelector('.resetBtnModal')
+const addBtn = document.querySelector('#addBtn')
 // 
+
+//  ====    OPEN/CLOSE ADD MODAL    ====   //    //
+addPokemonBtn.addEventListener('click', function(){
+    addBtnModal.style.display = 'block'
+})
+
+closeBtn.addEventListener('click', function(){
+    addBtnModal.style.display = 'none'
+})
+
+// ==== OPEN/CLOSE DELETE MODAL ====//
+deleteBtn.addEventListener('click', function(){
+    deleteBtnModal.style.display = 'block'
+})
+
+deleteCloseBtn.addEventListener('click', function(){
+    deleteBtnModal.style.display = 'none'
+})
+
+// ==== OPEN/CLOSE SHOW MODAL ====//
+showBtn.addEventListener('click', function(){
+    showBtnModal.style.display = 'block'
+})
+
+showCloseBtn.addEventListener('click', function(){
+    showBtnModal.style.display = 'none'
+})
+
+// ==== OPEN/CLOSE RESET MODAL ====//
+resetBtn.addEventListener('click', function(){
+    resetBtnModal.style.display = 'block'
+})
+
+resetCloseBtn.addEventListener('click', function(){
+    resetBtnModal.style.display = 'none'
+})
